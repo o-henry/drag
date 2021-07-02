2da1531bc80eacf1fb6aec1c6d8e314d0822c64a
henry <cksgod16@gmail.com>
2021-07-02 22:02:49 +0900

    Update highlight.js - maintain color

--- src/content/highlight.js
+++ src/content/highlight.js
@@ -1,12 +1,33 @@
 /**
  * Inject drag into the current page
  */
-
 document.addEventListener('mouseup', () => {
-  let select = window.getSelection().toString();
+  let select = window.getSelection
+    ? window.getSelection()
+    : document.selection.createRange();
 
-  if (select.length) {
-    chrome.runtime.sendMessage({ message: 'text', data: select });
-    setCookie('highlight', select, 7);
+  if (select) {
+    /* this feature is obsolete but
+     * https://github.com/yabwe/medium-editor/blob/master/src/js/core.js#L47
+     * https://github.com/codex-team/editor.js/blob/master/src/components/inline-tools/inline-tool-bold.ts#L73
+     * https://imgur.com/a/Y90otfU
+     * are using it.
+     * */
+
+    window.document.designMode = 'On';
+    window.document.execCommand('hiliteColor', false, 'yellow');
+  }
+
+  if (select.toString().length) {
+    chrome.runtime.sendMessage({ message: 'text', data: select.toString() });
+    setCookie('highlight', select.toString(), 7);
   }
 });
+
+/**
+ * background
+ * let range = select.getRangeAt(0);
+ * let span = document.createElement('span');
+ * span.className = 'highlight';
+ * range.surroundContents(span);
+ */
